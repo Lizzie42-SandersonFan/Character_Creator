c28022b71f26a8ef72ea8eea86ccfb20279a74d8
lulu094 <lucci.venanciomostaceli@ucas-eu.net>
2026-01-21 09:51:39 -0700

    se hizo lo que se pudo

---
+++ main_file.py
@@ -0,0 +1,10 @@
+# Main Menu
+# dictionary with options
+# User has to choose a number to select an option
+# Each option is associated with a number for easy selection
+# What it would display
+# 1. View Characters
+# 2. Create Character
+# 3. Edit Character
+# 4. Exit
+# menu_option is set to input type 1 to 4 to decide which option to choose
